6edfa8031e3d462ff219b83bc7c6c2ab5ca89633
Vasilis Spilka <vasspilka@gmail.com>
2013-12-24 14:39:57 +0200

    depoy rename task

--- Capfile
+++ Capfile
@@ -9,18 +9,14 @@ require 'capistrano/deploy'
 # For documentation on these, see for example:
 #
 #   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
 #   https://github.com/capistrano/bundler
 #   https://github.com/capistrano/rails/tree/master/assets
 #   https://github.com/capistrano/rails/tree/master/migrations
-#
- require 'capistrano/rvm'
-# require 'capistrano/rbenv'
-# require 'capistrano/chruby'
- require 'capistrano/bundler'
- require 'capistrano/rails/assets'
- require 'capistrano/rails/migrations'
+
+require 'capistrano/rvm'
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repo_url, 'https://github.com/vasspilka/LocalGrowth'
 set :branch, 'master'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :deploy_to, "/home/user/apps/www/#{application}"
+set :deploy_to, "/home/user/apps/www/lamiatodo"
 set :scm, :git
 set :deploy_via, :remote_cache
 set :use_sudo, false
@@ -12,13 +12,14 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/application.yml}
+#set :linked_files, %w{config/database.yml}
+#set :linked_files, %w{config/application.yml}
 
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
+before "deploy:migrate", "configure:application"
 
 namespace :deploy do
  task :start do ; end
@@ -26,4 +27,10 @@ namespace :deploy do
  task :restart do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
  end
+end
+
+namespace :configure do
+  task :application do
+  	run "#{try_sudo} cp #{current_path}/config/application.example.yml #{current_path}/config/application.yml"
+  end
 end
