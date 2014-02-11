6d58c664a83bfdfe9bc1af4f046afbc2125d6a22
Vasilis Spilka <vasspilka@gmail.com>
2014-02-11 15:20:36 +0200

    mailer

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationsMailer < ActionMailer::Base
-  default from: CONFIG[:default_from]
+  default :from => CONFIG[:default_from]
   default :to => CONFIG[:default_to]
   def new_message(feedback_message)
     @feedback_message = feedback_message
