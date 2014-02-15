1ff89c4165e17232fda34c1b83ac85af9b2bcf34
Vasilis Spilka <vasspilka@gmail.com>
2014-02-15 21:53:10 +0200

    admin fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require_tree .
 
 
 $(function(){ $(document).foundation(); });
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,7 +9,7 @@ RailsAdmin.config do |config|
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = [CONFIG[:app_name], 'Admin']
+  config.main_app_name = ['Lamiatodo', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
